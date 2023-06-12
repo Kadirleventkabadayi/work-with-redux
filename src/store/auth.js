a56441944db877a738e699de99dc58ec1496907e
Prelencos <120386410+Kadirleventkabadayi@users.noreply.github.com>
2023-06-12 16:37:07 +0300

    updaated

--- src/store/auth.js
+++ src/store/auth.js
@@ -3,7 +3,6 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialAuthState = {
   isAuthenticated: false,
 };
-
 const authSlice = createSlice({
   name: "authentication",
   initialState: initialAuthState,
